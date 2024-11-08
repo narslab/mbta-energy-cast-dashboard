87c60ab3d3967f5c54cdb6f26c8fe9b0598152e7
Zhuo Han <64714994+BlankSpace-Hz@users.noreply.github.com>
2024-11-08 11:35:39 -0500

    Editions on the layout

--- mbta-energy-cast-dashboard.py
+++ mbta-energy-cast-dashboard.py
@@ -9,9 +9,6 @@ import os
 import scipy.stats as stats
 import tensorflow as tf
 
-print(tf.__version__)
-
-
 # Load the model and data
 model = load_model('models/test_model.h5')
 estimate_dist = pd.read_csv("data/tidy/estimated_distributions_planning_metrics.csv")
@@ -20,10 +17,31 @@ df_test = pd.read_csv("data/tidy/test_data.csv")
 # Set the wide representation as the deafult
 st.set_page_config(layout="wide")
 # Set up Streamlit
-st.title("MBTA energy cast")
+st.title("MBTA EnergyCast Dashboard")
 # st.write("")
-st.markdown("<p style='font-size: 18px; font-weight: bold;'>Sustainable energy forecasting tool for MBTA URT system</p>", unsafe_allow_html=True)
-
+st.markdown("<p style='font-size: 18px; font-weight: bold;'>A energy forecasting tool for the Massachusetts Bay Transportation Authority urban rail transit system (Boston T)</p>", unsafe_allow_html=True)
+st.markdown(
+    """
+    <p style="font-size: 18px; margin-top: 20px;">
+    This planning tool allows you to specify desired planning metrics for the MBTA URT for each line. 
+    When you click on the "Predict" button, it runs a long short-term memory network (LSTM) model to 
+    generate forecasts for the next 90 days. The current model has been trained on data from 01-2019 to 09-2021.
+    </p>
+    """,
+    unsafe_allow_html=True
+)
+# mitigate the button
+st.markdown(
+    """
+    <style>
+    .stButton > button {
+        font-size: 20px;
+        padding: 20px 40px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 # Create two columns
 col1, col2 = st.columns(2)
 
@@ -49,10 +67,10 @@ with col1:
 
     # Display headers for the metrics
     col_label, col_trips, col_distance, col_speed = st.columns([1, 2, 2, 2])
-    col_label.markdown("<p style='font-size: 35px; font-weight: bold;'>Line</p>", unsafe_allow_html=True)
-    col_trips.markdown("<p style='font-size: 35px; font-weight: bold;'>Number of Trips</p>", unsafe_allow_html=True)
-    col_distance.markdown("<p style='font-size: 35px; font-weight: bold;'>Operating Distance (miles)</p>", unsafe_allow_html=True)
-    col_speed.markdown("<p style='font-size: 35px; font-weight: bold;'>Average Speed (mph)</p>", unsafe_allow_html=True)
+    col_label.markdown("<p style='font-size: 18px; font-weight: bold;'>Line</p>", unsafe_allow_html=True)
+    col_trips.markdown("<p style='font-size: 18px; font-weight: bold;'>Number of Trips</p>", unsafe_allow_html=True)
+    col_distance.markdown("<p style='font-size: 18px; font-weight: bold;'>Operating Distance (miles)</p>", unsafe_allow_html=True)
+    col_speed.markdown("<p style='font-size: 18px; font-weight: bold;'>Average Speed (mph)</p>", unsafe_allow_html=True)
 
     # Initialize a dictionary to store the metrics
     metrics = {}
@@ -63,7 +81,7 @@ with col1:
         col_label, col_trips, col_distance, col_speed = st.columns([1, 2, 2, 2])
         
         # Display the line name in the first column
-        col_label.markdown(f"<p style='font-size: 18px; font-weight: bold;'>{line}</p>", unsafe_allow_html=True)
+        col_label.markdown(f"<p style='font-size: 15px; font-weight: bold;'>{line}</p>", unsafe_allow_html=True)
         
         # Display the input fields in the remaining columns
         num_trips = col_trips.number_input('', key=f"{line}_num_trips", value=default_values["num_trips"][i])
@@ -72,9 +90,34 @@ with col1:
         
         # Store the values in the metrics dictionary
         metrics[line] = {'Num_trips': num_trips, 'Dist': distance, 'Avg_speed': speed}
+
+    # Text before the logos
+    st.markdown(
+        """
+        <p style="font-size: 16px; margin-top: 20px;">
+        This tool was developed by the Networks for Accessibility, Resilience and Sustainability Laboratory (NARS Lab) at 
+        the University of Massachusetts Amherst, in partnership with the Massachusetts Bay Transportation Authority (MBTA). 
+        The project was funded by the Massachusetts Department of Transportation (MassDOT), and the grant was administered 
+        by the University of Massachusetts Transportation Center (UMTC).
+        </p>
         
-    # Prediction button
-    predict_button = st.button("Predict")
+        <p style="font-size: 16px; font-weight: bold; margin-top: 20px;">Credits:</p>
+        <ul style="font-size: 15px;">
+            <li>Lead Researcher and Developer: Zhuo Han (NARS Lab, UMass)</li>
+            <li>Co-Principal Investigators: Dr. Eleni Christofa and Dr. Eric Gonzales (UMass)</li>
+            <li>Principal Investigator: Dr. Jimi Oke (NARS Lab, UMass)</li>
+            <li>Project Champion: Sean Donaghy (MBTA)</li>
+            <li>Project Manager: Michael Flanary (MassDOT)</li>
+            <li>Administrative Support: Kimberley Foster, Matt Mann, Michelle Clark (UMTC) and Anil S. Gurcan (MassDOT)</li>
+        </ul>
+
+        <p style="font-size: 15px; font-weight: bold;">
+            <a href="https://github.com/narslab/mbta-energy-cast-dashboard.git" target="_blank">GitHub Repository: MBTA EnergyCast Dashboard</a>
+        </p>
+        """, 
+        unsafe_allow_html=True
+    )
+
     # Display images from the 'logo' folder below the inputs
     logo_path = "logo"  # Path to the 'logo' folder
     image_width = 200  # Set a fixed width for all images
@@ -102,69 +145,69 @@ with col1:
         if os.path.exists(image_path):
             col.image(image_path, width=image_width)
 
-# Run predictions only when the button is clicked
-if predict_button:
-    # Data generation process
-    estimate_dist['parameters'] = estimate_dist['parameters'].apply(ast.literal_eval)
+with col2:
+    # Prediction button
+    predict_button = st.button("Predict")
+    # Run predictions only when the button is clicked
+    if predict_button:
+        # Data generation process
+        estimate_dist['parameters'] = estimate_dist['parameters'].apply(ast.literal_eval)
+        def generate_data(dist_name, params, size=90):
+            dist = getattr(stats, dist_name)
+            return dist.rvs(size=size, **params)
+        generated_data = {'route_id': [], 'metric': [], 'generated_values': []}
+        for _, row in estimate_dist.iterrows():
+            route_id = row['route_id']
+            metric = row['metric']
+            dist_name = row['distribution']
+            params = row['parameters']
+            data = generate_data(dist_name, params)
+            generated_data['route_id'].append(route_id)
+            generated_data['metric'].append(metric)
+            generated_data['generated_values'].append(data)
+        generated_data_df = pd.DataFrame(generated_data)
 
-    def generate_data(dist_name, params, size=90):
-        dist = getattr(stats, dist_name)
-        return dist.rvs(size=size, **params)
+        # Update planning variables based on generated data
+        planning_var = pd.DataFrame(metrics).T.reset_index()
+        planning_var.columns = ['Line', 'Num_trips', 'Dist', 'Avg_speed']
+        planning_var = pd.concat([planning_var] * 90, ignore_index=True)
 
-    generated_data = {'route_id': [], 'metric': [], 'generated_values': []}
-    for _, row in estimate_dist.iterrows():
-        route_id = row['route_id']
-        metric = row['metric']
-        dist_name = row['distribution']
-        params = row['parameters']
-        data = generate_data(dist_name, params)
-        generated_data['route_id'].append(route_id)
-        generated_data['metric'].append(metric)
-        generated_data['generated_values'].append(data)
-    generated_data_df = pd.DataFrame(generated_data)
+        metric_to_column = {'trip_diff': 'Num_trips', 'dist_diff': 'Dist', 'avg_speed_diff': 'Avg_speed'}
+        for _, row in generated_data_df.iterrows():
+            line = row['route_id']
+            metric = row['metric']
+            generated_values = row['generated_values']
+            column = metric_to_column[metric]
+            line_mask = planning_var['Line'] == line
+            planning_var.loc[line_mask, column] += generated_values[:line_mask.sum()]
 
-    # Update planning variables based on generated data
-    planning_var = pd.DataFrame(metrics).T.reset_index()
-    planning_var.columns = ['Line', 'Num_trips', 'Dist', 'Avg_speed']
-    planning_var = pd.concat([planning_var] * 90, ignore_index=True)
+        planning_var_reshaped = planning_var.pivot_table(index=planning_var.index // 5, columns='Line', values=['Num_trips', 'Dist', 'Avg_speed']).reset_index(drop=True)
+        planning_var_reshaped.columns = [f"{metric}_{line}" for metric, line in planning_var_reshaped.columns]
 
-    metric_to_column = {'trip_diff': 'Num_trips', 'dist_diff': 'Dist', 'avg_speed_diff': 'Avg_speed'}
-    for _, row in generated_data_df.iterrows():
-        line = row['route_id']
-        metric = row['metric']
-        generated_values = row['generated_values']
-        column = metric_to_column[metric]
-        line_mask = planning_var['Line'] == line
-        planning_var.loc[line_mask, column] += generated_values[:line_mask.sum()]
+        # Combine data for model input
+        combined_data = pd.concat([planning_var_reshaped.reset_index(drop=True), df_test.reset_index(drop=True)], axis=1)
 
-    planning_var_reshaped = planning_var.pivot_table(index=planning_var.index // 5, columns='Line', values=['Num_trips', 'Dist', 'Avg_speed']).reset_index(drop=True)
-    planning_var_reshaped.columns = [f"{metric}_{line}" for metric, line in planning_var_reshaped.columns]
+        # Preprocess data for model input
+        seq_data = combined_data[['Energy', 'TAVG']]
+        non_seq_data = combined_data.drop(['Energy', 'TAVG'], axis=1)
+        non_seq_data = non_seq_data.loc[:, non_seq_data.columns != 'Unnamed: 0']
 
-    # Combine data for model input
-    combined_data = pd.concat([planning_var_reshaped.reset_index(drop=True), df_test.reset_index(drop=True)], axis=1)
+        seq_scaler = StandardScaler()
+        non_seq_scaler = StandardScaler()
+        seq_data_scaled = seq_scaler.fit_transform(seq_data)
+        non_seq_data_scaled = non_seq_scaler.fit_transform(non_seq_data)
 
-    # Preprocess data for model input
-    seq_data = combined_data[['Energy', 'TAVG']]
-    non_seq_data = combined_data.drop(['Energy', 'TAVG'], axis=1)
-    non_seq_data = non_seq_data.loc[:, non_seq_data.columns != 'Unnamed: 0']
+        # Convert sequential data into sequences of 10 time steps
+        time_steps = 10
+        seq_input = np.array([seq_data_scaled[i:i + time_steps] for i in range(len(seq_data_scaled) - time_steps + 1)])
+        non_seq_input = non_seq_data_scaled[time_steps - 1:]
 
-    seq_scaler = StandardScaler()
-    non_seq_scaler = StandardScaler()
-    seq_data_scaled = seq_scaler.fit_transform(seq_data)
-    non_seq_data_scaled = non_seq_scaler.fit_transform(non_seq_data)
+        # Make predictions
+        predictions = model.predict([seq_input, non_seq_input])
+        predictions_original_scale = seq_scaler.inverse_transform(predictions)
+        energy_values = predictions_original_scale[:, 0]
+        temp_values = predictions_original_scale[:, 1]
 
-    # Convert sequential data into sequences of 10 time steps
-    time_steps = 10
-    seq_input = np.array([seq_data_scaled[i:i + time_steps] for i in range(len(seq_data_scaled) - time_steps + 1)])
-    non_seq_input = non_seq_data_scaled[time_steps - 1:]
-
-    # Make predictions
-    predictions = model.predict([seq_input, non_seq_input])
-    predictions_original_scale = seq_scaler.inverse_transform(predictions)
-    energy_values = predictions_original_scale[:, 0]
-    temp_values = predictions_original_scale[:, 1]
-
-    with col2:
         # First row with the energy and temperature forecast plots
         st.header("Forecast")
         fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 10))
